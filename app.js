cbd8e16c289f644eb791242a8f02a4b68b6beaa1
Jakub Myrta <98383163+mkuba2006@users.noreply.github.com>
2023-02-19 00:43:10 +0100

    w

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-a
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    const cd = new Date().getDay();
+    const day ='';
+
+    if(cd === 0){
+        day = 'Sunday';
+    }else if(cd === 1){
+        day = 'Monday';
+    }else if(cd === 2){
+        day = 'Tuesday';
+    }else if(cd === 3){
+        day = 'Wednesday';
+    }else if(cd === 4){
+        day = 'Thursday';
+    }else if(cd === 5){
+        day = 'Friday';
+    }else if(cd === 6){
+        day = 'Saturday';
+    }
+
+    res.render('app', {kind: day});
+})
 
 
-
-
-
-
-
-
-
-
+app.listen(3000, function(){
+    console.log('Starter');
+})
